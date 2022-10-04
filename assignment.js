180bf29ec958e5def1c953c04af6ea3dfa002830
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-10-04 12:50:04 +0530

    First program execution in JS
    First program execution in JS

---
+++ assignment.js
@@ -0,0 +1,6 @@
+console.log("Pradnya Bapusaheb Kolase");
+console.log("My Hobbies: 1)Drawing 2)Gardening 3)Reading");
+console.info("Personal Strength are as below: 1)Self-motivated  2)Hardworking 3)Honesty");
+console.warn("Weekness are as below: 1)Impatient  2)Too Detail Oriented  3)Not Flexible");
+console.log("Addition of two given number:-"+(30+50));
+console.log("Multiplication of two given number:-"+(49*479));
