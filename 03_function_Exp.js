ce80fd9bd25f6fa3f5525ee0f64b8863a9df72a7
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-10-10 15:45:37 +0530

    function expression assignment
    function expression assignment

---
+++ 03_function_Exp.js
@@ -0,0 +1,10 @@
+var test=function(argu1, argu2)
+{
+   console.log("Addition of two number  :-"  +  (argu1+argu2));
+   return "result";
+}
+var res=test(10,20);
+console.log(res);
+
+var var_name=10;
+console.log(var_name);
