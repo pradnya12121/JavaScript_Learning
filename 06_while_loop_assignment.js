2a48ce688d73b16e49dab537bd75fc5d17689a29
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-10-18 14:56:52 +0530

    update
    update

--- 06_while_loop_assignment.js
+++ 06_while_loop_assignment.js
@@ -1,6 +1,6 @@
 console.log("1.WAP to print number from 5 t0 15 by incrementing 1 using while loop");
-var count=4;
-var re=" ";
+var count=5;
+var re=" 5";
 while (count<15) {
     count++
     re=re + " " +count;
@@ -10,8 +10,8 @@ console.log(`${re}`);
 console.log("===================================================================================");
 
 console.log("2 .WAP to print number from 50 to 40 by decrementing by 1 using do while loop");
-var count=51;
-var res=" ";
+var count=50;
+var res="50 ";
 do { count--
     res=res + " "+count;
    
@@ -21,7 +21,7 @@ console.log(`${res}`)
 console.log("=================================================================================");
  console.log("3.WAP to find first 15 odd number using while loop");
  var count=1;
- var sto=  " ";
+ var sto=  " 1";
  while (count<29) {
     count=count + 2;
     sto=sto +" " + count;
@@ -42,8 +42,8 @@ console.log("===================================================================
  console.log(pqr);
  console.log("==============================================================================");
  console.log("5. WAP to print table of 5 like --->5 10 15");
- var count=0;
- var rmt= " ";
+ var count=5;
+ var rmt= " 5";
  while (count<50) {
     count=count+5;
     rmt=rmt + " " +count;
@@ -62,8 +62,8 @@ console.log("===================================================================
  console.log(lmn);
  console.log("==================================================================================");
  console.log("7. WAP to print number from 100 to 10")
- var count=110;
- var pdf=" ";
+ var count=100;
+ var pdf="100 ";
  while (count>10) {
     count=count - 10;
     pdf=pdf + " "+count;
