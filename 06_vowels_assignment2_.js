2070dde574476c337298c9986150c396f8b38971
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-10-18 14:19:43 +0530

    while loop assignment
    while loop assignment

--- 06_vowels_assignment2_.js
+++ 06_vowels_assignment2_.js
@@ -14,3 +14,23 @@ function reverseString(arg)
 reverseString("Hard work always pays back");
 console.log("=========================================================================")
 reverseString("Soon I will be Angular IT Champ")
+console.log("=========================================================================")
+
+function reverseString1(arg)
+{
+   var rev =arg.length-1;
+  var sto1=" ";
+   for (let index = rev; index >=0; index--) 
+   {
+    if((arg.charAt(index))!== " ")
+ sto1=sto1 + arg.charAt(index)
+    
+   }
+   console.log(sto1);
+
+}
+
+reverseString1("Hard work always pays back");
+console.log("=========================================================================")
+reverseString1("Soon I will be Angular IT Champ")
+console.log("=========================================================================")
--- 06_while_loop_assignment.js
+++ 06_while_loop_assignment.js
@@ -1,22 +1,73 @@
 console.log("1.WAP to print number from 5 t0 15 by incrementing 1 using while loop");
 var count=4;
+var re=" ";
 while (count<15) {
     count++
-    console.log(`${count}`);
+    re=re + " " +count;
+       
 }
-
+console.log(`${re}`);
 console.log("===================================================================================");
 
 console.log("2 .WAP to print number from 50 to 40 by decrementing by 1 using do while loop");
-var count=51
+var count=51;
+var res=" ";
 do { count--
-    console.log(`${count}`)
+    res=res + " "+count;
+   
 } while (count>40);
+console.log(`${res}`)
+
 console.log("=================================================================================");
  console.log("3.WAP to find first 15 odd number using while loop");
  var count=1;
- while (count<=30) {
-    count=count+2;
-    console.log(`${count}`);
+ var sto=  " ";
+ while (count<29) {
+    count=count + 2;
+    sto=sto +" " + count;
+   
     
  }
+ console.log(sto);
+ console.log("=============================================================================");
+ console.log("4.WAP to find first 10 even numbers :");
+ var count=0;
+ var pqr= " ";
+ while (count<=18) {
+    count=count+2;
+    pqr=pqr + " " + count;
+
+    
+ }
+ console.log(pqr);
+ console.log("==============================================================================");
+ console.log("5. WAP to print table of 5 like --->5 10 15");
+ var count=0;
+ var rmt= " ";
+ while (count<50) {
+    count=count+5;
+    rmt=rmt + " " +count;
+    
+ }
+ console.log(rmt);
+ console.log("==================================================================================");
+ console.log("6. WAP to print number from 10 to 100");
+ var count=0;
+ var lmn= " ";
+ while (count<100) {
+    count=count+10;
+    lmn=lmn + " "+count;
+    
+ }
+ console.log(lmn);
+ console.log("==================================================================================");
+ console.log("7. WAP to print number from 100 to 10")
+ var count=110;
+ var pdf=" ";
+ while (count>10) {
+    count=count - 10;
+    pdf=pdf + " "+count;
+    
+ }
+ console.log(pdf);
+ 
