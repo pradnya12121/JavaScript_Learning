d41b34918615b64ca250198321bb5f992ab86580
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-10-20 11:33:49 +0530

    09_Object_Assignment
    09_Object_Assignment

---
+++ 09_object.js
@@ -0,0 +1,39 @@
+let objectSachin = {
+    fullName : "Sachin",
+    city:"mumbai",
+    age:42,
+    address:{
+        street:"As Club",
+        pinCode:12345,
+        city:"Pune",
+        state:"MH",
+    },
+      eat:function()
+    {
+     let personDetail = `Details are: ${this.fullName}  ${this.city}   ${this.address}`
+     return personDetail
+    }
+    
+
+}
+
+console.log(Object.entries(objectSachin))
+console.log(Object.keys(objectSachin))
+console.log(Object.values(objectSachin))
+let pDetail = objectSachin.eat()
+console.log(`${pDetail}`)
+console.log("=================================")
+objectSachin.eat();
+
+objectSachin.address.landmark="Durga temple";
+console.log(objectSachin);
+let personname=objectSachin.fullName;
+console.log(personname);
+console.log(objectSachin["fullName"]);
+objectSachin.pinCode=123456;
+console.table(objectSachin);
+objectSachin.profession="Angulaer Developer";
+objectSachin.fullName="Sachin Ramesh Tendulkar";
+delete objectSachin.age;
+console.log(objectSachin.address.city);
+console.table(objectSachin);
---
+++ 09_object_assignment.js
@@ -0,0 +1,58 @@
+let teacher = {
+    FullName :"Smita Gore",
+    Qualification : "Msc",
+    // Age :45,
+    contactNo :8104834562,
+     degree:{
+        PHD : "Adv Computing",
+        BEd  : "Maths",
+        Engineering :"Electronics & Telecommunication",
+        MEd :"Chemistry",
+        certificate :{
+            certificateName :"TEFL Certificate",
+            certificateName2 :"Certificate in Advance Computing",
+            certificateName3 :"Certificate in IFE course",
+            certificateName4  :"Certificate in Hacker Rank",
+
+        },
+        teaching:function(){
+            let allDegree =  (`Teacher Degrees are :${this.PHD}   ${this.BEd}   ${this.Engineering}   ${this.MEd}`)
+         return allDegree
+      
+          }
+         
+    
+    }
+    
+   
+}
+console.log("");
+ let aDegree =teacher.degree.teaching()
+ console.log(`${aDegree}`);
+ console.log("====================================================================")
+//  console.log("");
+ console.log(`Update New Property Age : 45`);
+teacher.Age=45;
+console.log(teacher);
+console.log("=====================================================================");
+console.log(`Modifiy Existing property  "FullName "`)
+teacher.FullName="Smita Ramesh Gore";
+console.log(teacher)
+console.log("=====================================================================");
+console.log("Delete any one Certificate from nested object");
+console.log("Total certificate before delete :")
+
+console.log(teacher.degree.certificate)
+console.log("====================")
+console.log("Total certificate After Delete");
+delete teacher.degree.certificate.certificateName2
+
+console.log(teacher.degree.certificate)
+console.log("======================================================================");
+console.log("Add new Certificate in nested Object");
+teacher.degree.certificate.certificateName5="Advance Computing";
+console.log(teacher.degree.certificate);
+
+
+
+// console.table(teacher);
