4ba42571bf08bfd32d97b44adfbe20d94ee32294
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-10-21 20:00:51 +0530

    09_date
    09_date

---
+++ 09_date.js
@@ -0,0 +1,15 @@
+let date= new Date();
+
+console.log(date);
+console.log(date.getDate());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+var dt=new Date("Aug 23,2005 23:30:00");
+console.log(dt);
+const d = new Date("2015-03-25");
+console.log(d);
+const dd=new Date("3-25-2016");
+console.log(dd);
+const ddd=new Date("25 march 2015");
+console.log(ddd);
+
