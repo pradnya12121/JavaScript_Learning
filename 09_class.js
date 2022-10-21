ff5726b3342b7e16db13d24acb5a5ad775cba8c3
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-10-21 18:26:53 +0530

    update class
    update class

--- 09_class.js
+++ 09_class.js
@@ -12,10 +12,11 @@ class Person{
 
 
 }
-sachin.details()
+// sachin.details();
 let sachin = new Person("Sachin Tendulkar", "Pune", 45, "Male");
+sachin.details();
 let rohit = new Person("Rohit Shram", "Mumbai", 32, "Male");
 console.log(sachin);
 console.log(rohit);
-console.log(s1 instanceof Student);
-console.log(sachin instanceof Student);
+// console.log(s1 instanceof Student);
+console.log(sachin instanceof Person);
